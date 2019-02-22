1b77c1b04c530dd4e4692523028d52f4a06dcc5c
Jim Blackler <jimblackler@google.com>
2019-02-22 14:03:44 +0000

    More development

--- tool3js.py
+++ tool3js.py
@@ -1,12 +1,8 @@
 import argparse
 import base64
+import hashlib
 import json
 import os
-
-
-def data_url_from(filename):
-  with open(filename, 'rb') as data:
-    return 'data:image/jpeg;base64,' + base64.b64encode(data.read())
 
 
 def main():
@@ -17,17 +13,34 @@ def main():
 
   args = parser.parse_args()
 
-  with open(args.input) as file:
-    scene = json.load(file)
+  base = os.path.dirname(args.input)
+
+  with open(args.input) as f:
+    scene = json.load(f)
+
+  hashes = {}
+  if args.urls == 'file':
+    for root, subdirs, files in os.walk(base):
+      for filename in files:
+        full_filename = os.path.join(root, filename)
+        bytes_read = open(full_filename, 'rb').read()
+        hashes[hashlib.sha256(bytes_read).hexdigest()] = os.path.relpath(full_filename, base)
 
   for image in scene['images']:
     if 'url' in image:
       url = image['url']
       if args.urls == 'data':
-        image['url'] = data_url_from(os.path.join(os.path.dirname(args.input), url))
+        with open(os.path.join(base, url), 'rb') as data:
+          image['url'] = 'data:image/jpeg;base64,' + base64.b64encode(data.read())
+      elif args.urls == 'file':
+        parts = url.split(';base64,', 1)
+        if len(parts) == 2:
+          digest = hashlib.sha256(base64.b64decode(parts[1])).hexdigest()
+          if digest in hashes:
+            image['url'] = hashes[digest]
 
-  with open(os.path.join(os.path.dirname(args.input), args.output), 'w') as file:
-    json.dump(scene, file, indent=2)
+  with open(os.path.join(base, args.output), 'w') as f:
+    json.dump(scene, f, indent=2)
 
 
 if __name__ == "__main__":
